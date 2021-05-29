a7e505e9d13ee555626869d23a708ff727342ec1
jaeltg <jaeltg@hotmail.com>
2021-05-29 13:44:51 +0100

    added tables and controllers to ward and waiting room

--- src/main/java/com/codeclan/example/The_Ward/models/rooms/Room.java
+++ src/main/java/com/codeclan/example/The_Ward/models/rooms/Room.java
@@ -1,15 +1,35 @@
 package com.codeclan.example.The_Ward.models.rooms;
 
+import com.codeclan.example.The_Ward.models.people.Patient;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@MappedSuperclass
 public abstract class Room {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name="capacity")
     private int capacity;
 
-    public Room(int capacity){
+    public Room(int capacity) {
         this.capacity = capacity;
     }
 
     public Room(){
 
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public int getCapacity() {
@@ -19,4 +39,6 @@ public abstract class Room {
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
+
+
 }

c1e58eef39791274f7a4a0d2c6fcafb11368fe37
calumpetergunn <calumgunn@gmail.com>
2021-05-29 11:02:09 +0100

    added ward and waiting room classes

--- src/main/java/com/codeclan/example/The_Ward/models/rooms/Room.java
+++ src/main/java/com/codeclan/example/The_Ward/models/rooms/Room.java
@@ -1,4 +1,22 @@
 package com.codeclan.example.The_Ward.models.rooms;
 
 public abstract class Room {
+
+    private int capacity;
+
+    public Room(int capacity){
+        this.capacity = capacity;
+    }
+
+    public Room(){
+
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }

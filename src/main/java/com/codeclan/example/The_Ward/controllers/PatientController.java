5094f6da4b34f85aa5de537edf333c842b6ee4ab
jaeltg <jaeltg@hotmail.com>
2021-05-29 11:58:37 +0100

    added all routes for patients illnesses specialists repos

--- src/main/java/com/codeclan/example/The_Ward/controllers/PatientController.java
+++ src/main/java/com/codeclan/example/The_Ward/controllers/PatientController.java
@@ -5,8 +5,7 @@ import com.codeclan.example.The_Ward.repositories.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +18,36 @@ public class PatientController {
     @GetMapping(value = "/patients")
     public ResponseEntity<List<Patient>> getAllPatients(){
         return new ResponseEntity<>(patientRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value="/patients/{id}")
+    public ResponseEntity getPatient(@PathVariable Long id) {
+        return new ResponseEntity<>(patientRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/patients")
+    public ResponseEntity<Patient> createPatient(@RequestBody Patient patient){
+        patientRepository.save(patient);
+        return new ResponseEntity<>(patient, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value="/patients/{id}")
+    public ResponseEntity<Patient> updatePatient(@RequestBody Patient patient, @PathVariable Long id){
+        Patient patientToUpdate = patientRepository.findById(id).get();
+        patientToUpdate.setName(patient.getName());
+        patientToUpdate.setAge(patient.getAge());
+        patientToUpdate.setPhoto(patient.getPhoto());
+        patientToUpdate.setHealth(patient.getHealth());
+        patientToUpdate.setIllness(patient.getIllness());
+        patientToUpdate.setStatus(patient.getStatus());
+        patientRepository.save(patientToUpdate);
+        return new ResponseEntity<>(patientToUpdate, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value="/patients/{id}")
+    public ResponseEntity<Long> deletePatient(@PathVariable Long id){
+        patientRepository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
 }
